f3c0d738a77884506cac57581254f8a4f284b167
Casey Timm <casey.m.timm@gmail.com>
2020-07-15 21:21:36 -0400

    updated to use updated cms

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import Posts from './posts'
-import ApolloClient from "apollo-boost"
-import { ApolloProvider } from "@apollo/react-hooks"
-import AuthProvider from "./authContext"
-import Banner from './Banner';
-import Footer from './Footer';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import "./App.css";
+import Posts from "./posts";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import AuthProvider from "./authContext";
+import Banner from "./Banner";
+import Footer from "./Footer";
+import { Grid } from "@material-ui/core";
 
 const client = new ApolloClient({
-  uri: "https://cms.caseytimm.com/graphql",
-  request: operation => {
-    const token = JSON.parse(localStorage.getItem("authBody"))
+  uri: "https://cms2.caseytimm.com/graphql",
+  request: (operation) => {
+    const token = JSON.parse(localStorage.getItem("authBody"));
     operation.setContext({
       headers: {
         authorization:
@@ -19,17 +19,17 @@ const client = new ApolloClient({
             ? `Bearer ${token.data.jwt}`
             : "",
       },
-    })
+    });
   },
-})
+});
 
-const Wrapper = props => (
+const Wrapper = (props) => (
   <AuthProvider>
     <ApolloProvider client={client}>
       <App props={props} />
     </ApolloProvider>
   </AuthProvider>
-)
+);
 
 function App() {
   return (
