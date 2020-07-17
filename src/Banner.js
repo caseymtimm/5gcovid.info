f4263ed6994e7ac4436c66bf6552e04dd696e6e1
cmtimm <cmtimm@safaricircuits.com>
2020-07-17 15:02:57 -0400

    Banner has two images and all banner info is from strapi

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,53 @@
-import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
-import useDimensions from "react-use-dimensions"
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import useDimensions from "react-use-dimensions";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import { Link } from "@reach/router";
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+const HEADER = gql`
+  {
+    fivegcoivdheader {
+      title
+      leftimage {
+        url
+      }
+      rightimage {
+        url
+      }
+    }
+  }
+`;
 
 export default () => {
-    const [ref, { height }] = useDimensions();
-    return (
-        <Grid container>
-            <Grid item xs={3}><img src="/Logo.svg" height={height} /></Grid>
-            <Grid item xs={9}><Typography variant="h1" component="h2" gutterBottom ref={ref}>5GCOVID.info</Typography> </Grid>
-        </Grid>
-    )
-}
+  const [ref, { height }] = useDimensions();
+  const { loading, error, data } = useQuery(HEADER);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <Grid container>
+      <Grid item xs={2}>
+        <img
+          src={`https://cms2.caseytimm.com${data.fivegcoivdheader.leftimage.url}`}
+          height={height}
+          alt="leftbanner"
+        />
+      </Grid>
+      <Grid item xs={8}>
+        <ButtonBase component={Link} to={`/`}>
+          <Typography variant="h1" component="h2" gutterBottom ref={ref}>
+            {data.fivegcoivdheader.title}
+          </Typography>
+        </ButtonBase>
+      </Grid>
+      <Grid item xs={2}>
+        <img
+          src={`https://cms2.caseytimm.com${data.fivegcoivdheader.rightimage.url}`}
+          height={height}
+          alt="rightbanner"
+        />
+      </Grid>
+    </Grid>
+  );
+};
